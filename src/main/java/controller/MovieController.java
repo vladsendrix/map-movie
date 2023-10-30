2835a1150e999ccad289f56cd422841ab6edb115
vladsendrix <vladsendrix@gmail.com>
2023-10-30 13:21:28 +0200

    ctrl: add inital implementation

---
+++ src/main/java/controller/MovieController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import domain.Movie;
+import repository.MovieRepository;
+
+import java.util.List;
+
+public class MovieController {
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void addMovie(Movie movie) {
+        movieRepository.add(movie);
+    }
+
+    public void viewAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        if (movies.isEmpty()) {
+            System.out.println("No movies available.");
+        } else {
+            for (Movie movie : movies) {
+                System.out.println(movieDetails(movie));
+            }
+        }
+    }
+
+    public void searchMovie(int movieID) {
+        Movie movie = movieRepository.findById(movieID);
+        if (movie != null) {
+            System.out.println(movieDetails(movie));
+        } else {
+            System.out.println("Movie not found.");
+        }
+    }
+
+    private String movieDetails(Movie movie) {
+        return "Movie ID: " + movie.getMovieID() +
+                ", Title: " + movie.getTitle() +
+                ", Director ID: " + movie.getDirectorID() +
+                ", Runtime: " + movie.getRunTimeMinutes() +
+                ", Budget: " + movie.getBudget() +
+                ", Year: " + movie.getYear();
+    }
+}
