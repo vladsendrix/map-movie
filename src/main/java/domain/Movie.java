6d7cbe84fe2d3a018be82742839a08e65a932bb1
vladsendrix <vladsendrix@gmail.com>
2023-10-30 13:13:53 +0200

    domain: refactor Movies

--- src/main/java/domain/Movie.java
+++ src/main/java/domain/Movie.java
@@ -3,28 +3,28 @@ package domain;
 public class Movie {
     private int movieID;
     private String title;
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     private int year;
     private int directorID;
     private int studioID;
     private int runTimeMinutes;
     private double budget;
 
-    public Movie(int movieID, String title, int direcctorID, int studioID, int runTimeMinutes, double budget, int year) {
+    public Movie(int movieID, String title, int direcctorID, int studioID, int runTimeMinutes, double budget,
+            int year) {
         this.movieID = movieID;
         this.title = title;
         this.directorID = direcctorID;
         this.studioID = studioID;
         this.runTimeMinutes = runTimeMinutes;
         this.budget = budget;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
         this.year = year;
     }
 
