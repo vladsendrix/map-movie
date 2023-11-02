dd5c874ea422ba8fb552914516e1f6a846d9c5db
vladsendrix <vladsendrix@gmail.com>
2023-11-02 15:22:52 +0200

    initial import of Spring Project

---
+++ src/main/java/mapmovie/MapMovieApplication.java
@@ -0,0 +1,13 @@
+package mapmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapMovieApplication.class, args);
+	}
+
+}
