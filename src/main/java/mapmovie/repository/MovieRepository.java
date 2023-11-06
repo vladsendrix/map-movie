d3f4ea99711e1814609c1d76baea504264b5132d
vladsendrix <vladsendrix@gmail.com>
2023-11-06 09:56:24 +0200

    repo: implement findByTitle method for Movie model

--- src/main/java/mapmovie/repository/MovieRepository.java
+++ src/main/java/mapmovie/repository/MovieRepository.java
@@ -2,8 +2,11 @@ package mapmovie.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import mapmovie.model.Movie;
 
 @Repository
 public interface MovieRepository extends BaseRepository<Movie, Integer> {
+    List<Movie> findByTitle(String title);
 }
