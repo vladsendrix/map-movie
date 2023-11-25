69eb29b2cc9da9a436c0b1a08e13fe7f836927f0
vladsendrix <vladsendrix@gmail.com>
2023-11-25 20:10:55 +0200

    remove unused imports

--- src/main/java/mapmovie/service/BaseService.java
+++ src/main/java/mapmovie/service/BaseService.java
@@ -1,6 +1,5 @@
 package mapmovie.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
